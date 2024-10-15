06bdf89a146c4d1b2f422e66aecdc21e811c137a
Ahsanullah Ahsan <ahsantani111@gmail.com>
2024-10-15 13:21:47 +0430

    Add files via upload
    This is a Python Python animation code using matpoltlib.

---
+++ matplotilb_animation.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
+
+import math
+import numpy as np
+
+count = 0
+def update(frameNum, a0):
+    global count
+    A = math.sin(math.radians(count))
+    x = [A*math.cos(a) for a in np.arange(0, 2*math.pi+0.1, 0.1)]
+    y = [math.sin(a) for a in np.arange(0, 2*math.pi+0.1, 0.1)]
+    a0.set_data(x, y)
+    count = (count + 1) % 360
+
+fig = plt.figure()
+ax = plt.axes(xlim=(-4, 4), ylim=(-1, 1))
+a0, = ax.plot([], [], color='red')  # Set the color to red
+anim = animation.FuncAnimation(fig, update, 
+                               fargs=(a0,), 
+                               interval=20)
+# show plot
+plt.show()
